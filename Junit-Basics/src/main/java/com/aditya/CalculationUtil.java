57e160d5449c52740bab2a6a2de275822f15b2c2
AdityaPawar6174 <adityabpawar393@gmail.com>
2025-07-26 20:09:27 +0530

    feat: add App.java and Calculation.java

---
+++ Junit-Basics/src/main/java/com/aditya/CalculationUtil.java
@@ -0,0 +1,32 @@
+package com.aditya;
+
+public class CalculationUtil {
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int multiply(int a, int b){
+        return a * b;
+    }
+
+    public String parseString(String n)
+    {
+        return n;
+    }
+
+    public int div(int a, int b)
+    {
+        return a/b;
+    }
+
+    public boolean isEven(int a)
+    {
+        return a % 2 == 0;
+    }
+
+    public boolean isOdd(int a)
+    {
+        return a % 2 != 0;
+    }
+}
