fa350824d902f2788011150f2752c5f3b4dafdc7
AdityaPawar6174 <adityabpawar393@gmail.com>
2025-07-28 19:48:56 +0530

    feat: add Operations.java class

---
+++ Junit-Basics/src/main/java/com/aditya/Operations.java
@@ -0,0 +1,10 @@
+package com.aditya;
+
+import java.util.stream.Stream;
+
+public class Operations {
+
+    public static Stream<String> checkMethodArgs(){
+        return  Stream.of("Hi", "Hello");
+    }
+}
